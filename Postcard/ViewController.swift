7029a1d677c13949964f82f5da73c392a8ac0204
goin18 <markobudal@gmail.com>
2014-10-06 12:26:46 +0200

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -6,10 +6,16 @@
 //  Copyright (c) 2014 Goin Developer. All rights reserved.
 //
 
-import UIKit
+import UIKit  //knjiznica kjer se vse nahaja
 
-class ViewController: UIViewController {
+class ViewController: UIViewController {     // ta class se klice ViewController in ima vse funkcije iz UIVC
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    //to je funkcija, ki nalizi aplikacijo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton)
+    {
+        NSLog("Moj string")
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()  //funkcija ki skrije tipkovnico
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+    }
 
 }
 
